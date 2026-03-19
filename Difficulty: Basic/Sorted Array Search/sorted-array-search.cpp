bc9f31bf4ba24648ae11459df2c92c4ad9ac0916
Dhruv Mishra <mdhruv.1909@gmail.com>
2026-03-19 18:42:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sorted Array Search/sorted-array-search.cpp
+++ Difficulty: Basic/Sorted Array Search/sorted-array-search.cpp
@@ -1,19 +1,12 @@
 class Solution {
   public:
     bool searchInSorted(vector<int>& arr, int k) {
-        // code here
         int n=arr.size();
-        bool result;
-        int count=0;
         for(int i=0;i<n;i++){
             if(arr[i]==k){
-                count++;
+                return true;
             }
         }
-        if(count>=1){
-            result=true;
-        }
-        else result=false;
-        return result;
+        return false;
     }
 };
